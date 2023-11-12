431e3bf42c17b72d09db9a1f17749362162a05bd
MMR <moj2raj2@gmail.com>
2023-11-13 02:47:18 +0330

    factor maker finished

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -668,7 +668,7 @@ class BotController extends Controller
                         $txt .= "name: $user->name\n";
                         $txt .= "telegram_username: $user->telegram_username\n";
                         $txt .= "telegram_id: $user->telegram_id\n";
-                        $txt .= "channels:" . join(Channel::where('user_id', $user->id)->pluck('chat_username')) . "\n";
+                        $txt .= "channels:" . join(',', Channel::where('user_id', $user->id)->pluck('chat_username')) . "\n";
 //                        $txt .= "groups: " . Group::where('user_id', $user->id)->pluck('chat_username') . "\n";
                         $txt .= "score: $user->score\n";
                         $txt .= "\n-----------------------\n";
