9c6efb85a5865c8d006c43fe341b2578fb5eab1f
MMR <moj2raj2@gmail.com>
2023-11-13 02:33:49 +0330

    factor maker finished

--- config/logging.php
+++ config/logging.php
@@ -43,13 +43,13 @@ return [
 
         'single' => [
             'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/' . date('m_d_Y', time()) . '.log'),
             'level' => env('LOG_LEVEL', 'debug'),
         ],
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/' . date('m_d_Y', time()) . '.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 14,
         ],
@@ -98,7 +98,9 @@ return [
         ],
 
         'emergency' => [
-            'path' => storage_path('logs/laravel.log'),
+            'emergency' => [
+                'path' => storage_path('logs/' . date('m_d_Y', time()) . '.log'),
+            ],
         ],
     ],
 
